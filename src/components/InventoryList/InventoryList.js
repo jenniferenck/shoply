5264a2ac2cd2c0caa6c37a45438fe2ad2aecaeb2
Jennifer Enck <jenniferenck@mac.com>
2019-03-29 07:33:59 -0700

    adjusted sizing space of inventory

--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -5,7 +5,7 @@ import inventory from '../../data.json';
 class InventoryList extends Component {
   render() {
     return (
-      <div className="container-fluid">
+      <div className="container-fluid col-9">
         <div className="row my-4 px-2">
           {inventory.items.map(item => (
             <InventoryItemContainer item={item} key={item.id} />
